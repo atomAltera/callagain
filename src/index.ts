77baa66281eaf8e80180b869439e049d53e62919
Konstantin Alikhanov <atomAltera@gmail.com>
2018-10-31 16:14:54 +0400

    Completely new design; Version: 1.0.0-beta.1

--- src/index.ts
+++ src/index.ts
@@ -1,122 +1,310 @@
-export type ResolveFunc<T> = (value: T) => void;
-export type RejectFunc = (reason?: any) => void;
-export type AgainFunc = (sleep?: number) => void;
-export type QueueFunc<T> = (resolve: ResolveFunc<T>, reject: RejectFunc, again: AgainFunc) => void;
+import Loki from 'lokijs';
 
-interface QueueEntry<T> {
-    func: QueueFunc<T>
-    resolve: ResolveFunc<T>
-    reject: RejectFunc
+export interface CallAgainOptions {
+    maxConcurrentCalls?: number
+    maxRetryAttempts?: number
+    delayOnRetry?: number
+    maxCallsPerInterval?: number
+    intervalLength?: number
 }
 
-interface CallAgainOptions {
-    defaultCycleDelay?: number
-    defaultRetryDelay?: number;
+export type NextHandler = (err: any) => boolean;
+export type ErrorHandler = (err: any, next: NextHandler) => boolean;
+
+type Args<F> = F extends (...args: infer T) => any ? T : never
+type Return<F> = F extends (...args: any[]) => infer T ? T : never
+type Promised<T> = T extends PromiseLike<any> ? T : Promise<T>
+
+enum CallEntryStatus {
+    init, waiting, processing, done
 }
 
-const DEFAULT_OPTIONS = {
-    defaultCycleDelay: 0,
-    defaultRetryDelay: 1000,
-};
+interface CallEntry {
+    id: number
+
+    func: Function
+    args: any
+
+    resolve?: (value: any) => void
+    reject?: (reason?: any) => void
+
+    status: CallEntryStatus
+
+    attempts: number
+    delay: number
+}
+
+interface CallHistory {
+    entryId: number
+    timestamp: number
+}
 
 export default class CallAgain {
-    private _queue: QueueEntry<any>[] = [];
+    private readonly _maxConcurrentCalls?: number;
+    private readonly _maxRetryAttempts?: number;
+    private readonly _delayOnRetry: number;
+    private readonly _maxCallsPerInterval?: number;
+    private readonly _intervalLength?: number;
 
-    private _cycleTimer?: number;
-    private _delayTimer?: number;
-    private _aborted = false;
+    private _cycleTimer?: number = undefined;
 
-    private readonly _options: CallAgainOptions;
+    private readonly _db: Loki;
+    private readonly _entries: Loki.Collection<CallEntry>;
+    private readonly _history: Loki.Collection<CallHistory>;
 
-    /**
-     * Create instance of CallAgain
-     * @param {CallAgainOptions} options - configuration options
-     */
-    constructor(options?: CallAgainOptions) {
-        this._options = {
-            ...DEFAULT_OPTIONS,
-            ...options
-        };
+    private _errorHandlers: ErrorHandler[] = [];
+    private _lastId = 0;
+
+    public constructor(options?: CallAgainOptions) {
+        this._maxConcurrentCalls = options && options.maxConcurrentCalls;
+        this._maxRetryAttempts = options && options.maxRetryAttempts;
+        this._delayOnRetry = options && options.delayOnRetry || 1000;
+        this._maxCallsPerInterval = options && options.maxCallsPerInterval;
+        this._intervalLength = options && options.intervalLength;
+
+        this._db = new Loki('callagain.json');
+        this._entries = this._db.addCollection('entries', {unique: ['id',], indices: ['status',]});
+        this._history = this._db.addCollection('history', {indices: ['timestamp',]});
 
         this._planNextCycle = this._planNextCycle.bind(this);
-        this._cycle = this._cycle.bind(this);
+        this._onCycleTimerTick = this._onCycleTimerTick.bind(this);
+
+        // Default error handler
+        this._errorHandlers.push(() => false);
     }
 
     /**
-     * Adds function to executions queue
-     * @param {function} func - function to execute
-     * @return {Promise} promise of *func* return value
+     * Creates new function from provided. New function is controlled by CallAgain
+     * @param {Function} f - function that needs to be controlled
+     * @return {Function} controlled function
      */
-    public add<T>(func: QueueFunc<T>): Promise<T> {
-        this._aborted = false;
-
-        return new Promise<T>((resolve, reject) => {
-            this._queue.unshift({func, resolve, reject});
-
-            this._planNextCycle();
-        });
+    public wrap<F extends Function>(f: F) {
+        return (
+            (...args: any) => {
+                return this._onWrapperCalled(f, args)
+            }
+        ) as (...args: Args<F>) => Promised<Return<F>>
     }
 
     /**
-     * Clears execution queue, cancels all calls
+     * Registers error handler
+     * @param handler
      */
-    public reset(): void {
-        this._aborted = true;
-
-        this._delayTimer && clearTimeout(this._delayTimer);
-        this._cycleTimer && clearTimeout(this._cycleTimer);
-
-        this._queue = [];
+    public onError(handler: ErrorHandler): this {
+        this._errorHandlers.unshift(handler);
+        return this;
     }
 
-
-    private _processEntry<T>(entry: QueueEntry<T>): Promise<void> {
-        return new Promise<void>((finish) => {
-
-            const retry = (delay?: number) => {
-                delay = delay || this._options.defaultRetryDelay;
-
-                this._delayTimer = setTimeout(() => {
-                    this._delayTimer = undefined;
-                    this._processEntry(entry).then(finish)
-                }, delay)
-            };
-
-            entry.func(
-                (v: T) => {
-                    finish();
-                    this._aborted || entry.resolve(v);
-                },
-                (r?: any) => {
-                    finish();
-                    this._aborted || entry.reject(r);
-                },
-                retry
-            );
-
-        });
-    }
-
+    /**
+     * Set timeout to call cycle method next time
+     * @private
+     */
     private _planNextCycle() {
-        if (this._aborted) {
-            return;
-        }
+        if (this._cycleTimer !== undefined) return; // next cycle already planned
 
-        if (this._cycleTimer !== undefined) {
-            return;
-        }
+        const timeout = this._calculateNextCycleDelay();
+        if (timeout === undefined) return; // nothing to process
 
-        this._cycleTimer = setTimeout(this._cycle, this._options.defaultCycleDelay)
+        this._cycleTimer = setTimeout(this._onCycleTimerTick, timeout);
     }
 
-    private _cycle() {
-        this._cycleTimer = undefined;
-        const entry = this._queue.pop();
+    private _calculateNextCycleDelay() {
+        const initEntriesCount = this._entries.count({status: CallEntryStatus.init});
+        const waitingEntriesCount = this._entries.count({status: CallEntryStatus.waiting});
 
-        if (entry === undefined) {
+        // nothing to process
+        if ((initEntriesCount === 0) && (waitingEntriesCount === 0)) return undefined;
+
+        // there is entries still in initialization process
+        if (waitingEntriesCount === 0) return 1;
+
+        // interval limit exceeded
+        if (this._calculateCallsLeftBeforeIntervalLimit() === 0) return this._intervalLength;
+
+        // concurrent limit exceeded
+        if (this._calculateCallsLeftBeforeConcurrentLimit() === 0) return undefined;
+
+        const now = Date.now();
+
+        // there is entries that need to be executed ASP
+        const plannedForNowCount = this._entries.count({status: CallEntryStatus.waiting, delay: {$lte: now}});
+        if (plannedForNowCount > 0) return 0;
+
+        // calculate time to closest by delay
+        const closestByDelay = this._entries.chain()
+            .find({status: CallEntryStatus.waiting})
+            .simplesort('delay')
+            .limit(1)
+            .data()[0]!;
+
+        const delay = closestByDelay.delay - now;
+
+        if (delay < 0) {
+            console.warn("delay < 0 in _calculateNextCycleDelay");
+            return 0;
+        }
+
+        return delay;
+    }
+
+    private _calculateCallsLeftBeforeConcurrentLimit() {
+        if (this._maxConcurrentCalls === undefined) return undefined;
+
+        const currentCalls = this._entries.count({status: CallEntryStatus.processing});
+
+        const callsLeft = this._maxConcurrentCalls - currentCalls;
+
+        if (callsLeft < 0) {
+            console.warn("callsLeft < 0 in _calculateCallsLeftBeforeConcurrentLimit");
+            return 0;
+        }
+
+        return callsLeft;
+    }
+
+    private _calculateCallsLeftBeforeIntervalLimit() {
+        if (this._maxCallsPerInterval === undefined) return undefined;
+        if (this._intervalLength === undefined) return undefined;
+
+        const intervalStartTime = Date.now() - this._intervalLength;
+
+        const callsMade = this._history.count({timestamp: {$gte: intervalStartTime}});
+
+        const callsLeft = this._maxCallsPerInterval - callsMade;
+
+        if (callsLeft < 0) {
+            console.warn("callsLeft < 0 in _calculateCallsLeftBeforeIntervalLimit");
+            return 0;
+        }
+
+        return callsLeft;
+    }
+
+    private _onCycleTimerTick() {
+        const waitingEntriesCount = this._entries.count({status: CallEntryStatus.waiting});
+
+        if (waitingEntriesCount === 0) {
+            this._cycleTimer = undefined;
             return
         }
 
-        this._processEntry(entry).then(this._planNextCycle)
+        let callsLeftBeforeConcurrentLimit = this._calculateCallsLeftBeforeConcurrentLimit();
+        let callsLeftBeforeIntervalLimit = this._calculateCallsLeftBeforeIntervalLimit();
+
+        if (callsLeftBeforeConcurrentLimit === undefined) callsLeftBeforeConcurrentLimit = waitingEntriesCount;
+        if (callsLeftBeforeIntervalLimit === undefined) callsLeftBeforeIntervalLimit = waitingEntriesCount;
+
+        const maxCallsBeforeLimit = Math.min(callsLeftBeforeConcurrentLimit, callsLeftBeforeIntervalLimit);
+
+        // Select entries calls
+        const entriesToProcess = this._entries.chain()
+            .find({
+                status: CallEntryStatus.waiting,
+                delay: {$lt: Date.now()}
+            })
+            .simplesort('id')
+            .limit(maxCallsBeforeLimit)
+            .data()
+        ;
+
+        // if nothing to do, just return
+        if (entriesToProcess.length === 0) {
+            this._cycleTimer = undefined;
+            this._planNextCycle();
+            return
+        }
+
+        // start processing
+        for (let entry of entriesToProcess) {
+            this._processEntry(entry);
+        }
+
+        // plan next cycle
+        this._cycleTimer = undefined;
+        this._planNextCycle();
+    }
+
+    private _processEntry(entry: CallEntry) {
+        entry.status = CallEntryStatus.processing;
+
+        // TODO: Extract somewhere
+        // TODO: Clean up somewhere
+        this._history.insert({entryId: entry.id, timestamp: Date.now()});
+
+        // TODO: Clean up done entries
+        Promise.resolve(entry.func.apply(undefined, entry.args))
+            .then(result => {
+                entry.status = CallEntryStatus.done;
+                entry.resolve!(result);
+            })
+            .catch(e => {
+                entry.attempts = entry.attempts + 1;
+
+                if ((this._maxRetryAttempts !== undefined) && (entry.attempts >= this._maxRetryAttempts)) {
+                    entry.status = CallEntryStatus.done;
+                    entry.reject!(e);
+                    return
+                }
+
+                if (this._shouldRetryOnError(e)) {
+                    entry.delay = Date.now() + this._delayOnRetry;
+                    entry.status = CallEntryStatus.waiting;
+                } else {
+                    entry.status = CallEntryStatus.done;
+                    entry.reject!(e);
+                }
+            })
+            .then(() => this._entries.update(entry))
+            .then(() => this._planNextCycle())
+        ;
+    }
+
+    private _shouldRetryOnError(err: any): boolean {
+        const getHandler = (index: number): NextHandler => {
+            const handler = this._errorHandlers[index];
+
+            return (err: any) => handler(err, getHandler(index + 1))
+        };
+
+        return getHandler(0)(err);
+    }
+
+    /**
+     * Is called by wrapped function
+     * @param {function} func - source function
+     * @param args - arguments passed to wrapped function
+     * @private
+     */
+    private _onWrapperCalled(func: Function, args: any) {
+        const id = this._getNewId();
+
+        this._entries.insert({
+            id,
+
+            func,
+            args,
+
+            resolve: undefined,
+            reject: undefined,
+
+            status: CallEntryStatus.init,
+
+            attempts: 0,
+            delay: 0,
+        });
+
+        this._planNextCycle();
+
+        return new Promise((resolve, reject) => {
+            this._entries.findAndUpdate({id}, e => {
+                e.resolve = resolve;
+                e.reject = reject;
+                e.status = CallEntryStatus.waiting;
+            });
+        });
+    }
+
+    private _getNewId() {
+        return ++this._lastId;
     }
 }
---
+++ test/call-again.test.ts
@@ -0,0 +1,46 @@
+import CallAgain from '../src';
+
+describe("Wrapper functions", () => {
+    test("calls wrapped function when needed", async () => {
+        const c = new CallAgain();
+
+        const func = jest.fn();
+
+        const wrapperFunc = c.wrap(func);
+
+        expect(func.mock.calls.length).toBe(0);
+        await wrapperFunc();
+        expect(func.mock.calls.length).toBe(1);
+    });
+
+    test("passes the same arguments", async () => {
+        const c = new CallAgain();
+
+        const func = jest.fn();
+
+        const expectedArgument = 'the-argument';
+
+        const wrapperFunc = c.wrap(func);
+
+        await wrapperFunc(expectedArgument);
+
+        expect(func.mock.calls[0][0]).toBe(expectedArgument);
+    });
+
+    test("returns the same result", async () => {
+        const c = new CallAgain();
+
+        const func = jest.fn();
+
+        const expectedReturnValue = 'the-return-value';
+        func.mockReturnValueOnce(expectedReturnValue);
+
+        const wrapperFunc = c.wrap(func);
+        const actualReturnValue = await wrapperFunc();
+
+        expect(actualReturnValue).toBe(expectedReturnValue);
+    });
+});
+
+
+
